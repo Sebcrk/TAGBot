d82116863125487019adfa2131d4a0c390430097
Sebcrk <60935077+Sebcrk@users.noreply.github.com>
2022-09-27 07:16:43 -0500

    Process is now ran if automatically every 10 minutes or if a get request is done. Found bug when clicking the On/Off button, as there is a loading time before the page appears again. Code to test a workaround is already in place, will test later today.

--- src/controllers/TAGWebController.js
+++ src/controllers/TAGWebController.js
@@ -4,107 +4,106 @@ const credentials = require("../../credentials.json");
 const loadTAGWeb = async (req, res) => {
   const { username, password, url } = credentials.TAGWeb;
 
-    console.log("\x1b[0m", "[started TAGWeb]");
+  console.log("\x1b[0m", "[started TAGWeb]");
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-      slowMo: 50,
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    slowMo: 50,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setUserAgent(
+    "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+  );
+
+  let redirect = false;
+  let isLoggedIn = false;
+
+  try {
+    /* ----------------------------------- Getting page ----------------------------------- */
+    await page.goto(url, { waitUntil: "networkidle0" });
+
+    /* ----------------------------------- Filling out username input ----------------------------------- */
+    console.log("-- Entering username");
+    await page.waitForSelector("#username");
+    await page.type("#username", username);
+    console.log("\x1b[0m", "Typed username");
+
+    /* ----------------------------------- Filling out Password input ----------------------------------- */
+    console.log("-- Entering Password");
+    await page.type("#Password", password);
+    console.log("\x1b[0m", "Typed Password");
+
+    /* ----------------------------------- Submitting credentials ----------------------------------- */
+    console.log("-- Submitting credentials");
+    await page.click("#btnLoginButton");
+    console.log("Submited credentials!");
+
+    /* ----------------------------------- Handle results ----------------------------------- */
+
+    // If there is another open session:
+    if ((await page.$("#DivSession")) !== null) {
+      console.log(
+        "You already have an open Session or you didn't close the previous one."
+      );
+      redirect = true;
+    }
+
+    redirect &&
+      (console.log("Clicking ~here~ to use this session"),
+      await page.screenshot({
+        path: "anotherSessionAlert.png", // Image Dimensions : 800 x 600
+      }),
+      await page.click("a", { delay: 100 }),
+      console.log("here button clicked"));
+
+    // If network error (not able to reproduce yet)
+
+    // If logIn succeds
+    await page.waitForSelector("#divEndSession > a");
+    /* ----------------------------------- Checking if user is logged in ----------------------------------- */
+
+    if ((await page.$("#btnLogout")) !== null) {
+      console.log("User is logged in");
+      await page.screenshot({
+        path: "loadedContent.png", // Image Dimensions : 800 x 600
+      });
+      isLoggedIn = true;
+    }
+
+    /* ----------------------------------- Logging out  ----------------------------------- */
+    isLoggedIn &&
+      (await page.click("#btnLogout", { delay: 1000 }),
+      console.log("Log Out button clicked"));
+
+    /* ----------------------------------- Logging in  ----------------------------------- */
+    !isLoggedIn &&
+      (await page.click("#btnLogin", { delay: 1000 }),
+      console.log("Log In button clicked"));
+
+    /* ----------------------------------- Disconnecting session  ----------------------------------- */
+    await page.waitForSelector("#divEndSession > a");
+    console.log("Clicking Disconnect button");
+    await page.click("#divEndSession > a", { delay: 200 });
+    console.log("Disconnect button clicked");
+
+    await page.screenshot({
+      path: "loggedOut.png", // Image Dimensions : 800 x 600
     });
 
-    const page = await browser.newPage();
-
-    await page.setUserAgent(
-      "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-    );
-
-    let redirect = false;
-    let isLoggedIn = false;
-
-    try {
-      /* ----------------------------------- Getting page ----------------------------------- */
-      await page.goto(url, { waitUntil: "networkidle0" });
-
-      /* ----------------------------------- Filling out username input ----------------------------------- */
-      console.log("-- Entering username");
-      await page.waitForSelector("#username");
-      await page.type("#username", username );
-      console.log("\x1b[0m", "Typed username");
-
-      /* ----------------------------------- Filling out Password input ----------------------------------- */
-      console.log("-- Entering Password");
-      await page.type("#Password", password );
-      console.log("\x1b[0m", "Typed Password");
-
-      /* ----------------------------------- Submitting credentials ----------------------------------- */
-      console.log("-- Submitting credentials");
-      await page.click("#btnLoginButton");
-      console.log("Submited credentials!");
-
-      /* ----------------------------------- Handle results ----------------------------------- */
-
-      // If there is another open session:
-      if ((await page.$("#DivSession")) !== null) {
-        console.log(
-          "You already have an open Session or you didn't close the previous one."
-        );
-        redirect = true;
-      }
-
-      redirect &&
-        (console.log("Clicking ~here~ to use this session"),
-        await page.screenshot({
-          path: "anotherSessionAlert.png", // Image Dimensions : 800 x 600
-        }),
-        await page.click("a", { delay: 100 }),
-        console.log("here button clicked"));
-
-      // If network error (not able to reproduce yet)
-
-      // If logIn succeds
-      await page.waitForSelector("#divEndSession > a");
-      /* ----------------------------------- Checking if user is logged in ----------------------------------- */
-
-      if ((await page.$("#btnLogout")) !== null) {
-        console.log("User is logged in");
-        await page.screenshot({
-          path: "loadedContent.png", // Image Dimensions : 800 x 600
-        });
-        isLoggedIn = true;
-      }
-
-      /* ----------------------------------- Logging out  ----------------------------------- */
-        isLoggedIn &&
-        (await page.click("#btnLogout", { delay: 1000 }),
-        console.log("Log Out button clicked"));
-
-      /* ----------------------------------- Logging in  ----------------------------------- */
-      // !isLoggedIn &&
-      //   (await page.click("#btnLogin", { delay: 1000 }),
-      //   console.log("Log In button clicked"));
-
-      /* ----------------------------------- Disconnecting session  ----------------------------------- */
-      console.log("Clicking Disconnect button");
-      await page.click("#divEndSession > a", { delay: 200 });
-      console.log("Disconnect button clicked");
-
-      await page.screenshot({
-        path: "loggedOut.png", // Image Dimensions : 800 x 600
-      });
-
-      /* ----------------------------------- Closing browser  ----------------------------------- */
-      await page.waitForSelector("#btnLoginButton");
-      console.log("Logged out. Closing browser");
-      await browser.close();
+    /* ----------------------------------- Closing browser  ----------------------------------- */
+    await page.waitForSelector("#btnLoginButton");
+    console.log("Logged out. Closing browser");
+    await browser.close();
     //   res.json("Bot completed the task successfully")
 
-      console.log("Browser closed");
-
-      
-    } catch (error) {
+    console.log("Browser closed");
+  } catch (error) {
     //   res.json("something bad happened")
-      console.error(error.message);
-    }
+    console.error(error.message);
+  }
 };
 
 module.exports = { loadTAGWeb };
--- src/server.js
+++ src/server.js
@@ -22,20 +22,24 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use(TAGWebRoute)
+const check = () => {
+  console.log("Checking the time...");
+const date = new Date().toTimeString().split(":")
+const time = `${date[0]}:${date[1]} ${date[2].split(" ")[1]}`
+const finalTime = time.split(" ")[0]
+console.log(finalTime);
+if((onTime === finalTime) || (offTime === finalTime) ) {
+  console.log("Bot running...");
+  loadTAGWeb()
+}  
+}
 
+check()
 setInterval(() => {
   // Check the time every minute
-  console.log("Checking the time...");
-  const date = new Date().toTimeString().split(":")
-  const time = `${date[0]}:${date[1]} ${date[2].split(" ")[1]}`
-  const finalTime = time.split(" ")[0]
-  
-  if((onTime === finalTime) || (offTime === finalTime) ) {
-    console.log("Bot running...");
-    loadTAGWeb()
-  }  
+check()
 
-}, 60000);
+}, 600000);
 
 app.listen(4200, () => console.log("Server started on port 4200"))
 
