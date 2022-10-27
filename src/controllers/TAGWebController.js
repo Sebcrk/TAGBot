e426ce7245154cf5ee6ddafb2636d69a7fad30bf
Sebcrk <60935077+Sebcrk@users.noreply.github.com>
2022-10-27 18:09:26 -0500

    Backed finished. Code works as expected. Bot runs by itself or when a GET request is made. Added a delay to ensure that loading time does not affect the process. Will possibly add a Frontend so other teammates can use it.

--- src/controllers/TAGWebController.js
+++ src/controllers/TAGWebController.js
@@ -1,6 +1,10 @@
 const puppeteer = require("puppeteer");
 const credentials = require("../../credentials.json");
 
+const delay = (seconds) => new Promise(resolve => {
+    setTimeout(resolve, seconds*1000)
+})
+
 const loadTAGWeb = async (req, res) => {
   const { username, password, url } = credentials.TAGWeb;
 
@@ -9,7 +13,7 @@ const loadTAGWeb = async (req, res) => {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    slowMo: 50,
+    slowMo: 10
   });
 
   const page = await browser.newPage();
@@ -18,51 +22,73 @@ const loadTAGWeb = async (req, res) => {
     "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
   );
 
-  let redirect = false;
+
   let isLoggedIn = false;
 
   try {
     /* ----------------------------------- Getting page ----------------------------------- */
-    await page.goto(url, { waitUntil: "networkidle0" });
+    await page.goto(url);
 
-    /* ----------------------------------- Filling out username input ----------------------------------- */
-    console.log("-- Entering username");
-    await page.waitForSelector("#username");
-    await page.type("#username", username);
-    console.log("\x1b[0m", "Typed username");
-
-    /* ----------------------------------- Filling out Password input ----------------------------------- */
-    console.log("-- Entering Password");
-    await page.type("#Password", password);
-    console.log("\x1b[0m", "Typed Password");
-
-    /* ----------------------------------- Submitting credentials ----------------------------------- */
-    console.log("-- Submitting credentials");
-    await page.click("#btnLoginButton");
-    console.log("Submited credentials!");
-
+        /* ----------------------------------- Filling out username input ----------------------------------- */
+        console.log("-- Entering username");
+        await page.waitForSelector("#username");
+        await page.type("#username", username);
+        console.log("\x1b[0m", "Typed username");
+      
+        /* ----------------------------------- Filling out Password input ----------------------------------- */
+        console.log("-- Entering Password");
+        await page.type("#Password", password);
+        console.log("\x1b[0m", "Typed Password");
+      
+        /* ----------------------------------- Submitting credentials ----------------------------------- */
+        console.log("-- Submitting credentials");
+        await page.click("#btnLoginButton");
+        console.log("Submited credentials!");
+    
     /* ----------------------------------- Handle results ----------------------------------- */
 
     // If there is another open session:
+    console.log(await page.$("#DivSession"))
     if ((await page.$("#DivSession")) !== null) {
       console.log(
         "You already have an open Session or you didn't close the previous one."
       );
-      redirect = true;
-    }
-
-    redirect &&
-      (console.log("Clicking ~here~ to use this session"),
+      console.log("Clicking ~here~ to use this session");
       await page.screenshot({
         path: "anotherSessionAlert.png", // Image Dimensions : 800 x 600
-      }),
-      await page.click("a", { delay: 100 }),
-      console.log("here button clicked"));
+      });
+      await page.click("a"), console.log("here button clicked");
+    }
 
     // If network error (not able to reproduce yet)
+    // if ((await page.$("div.")) !== null) {
+    //   console.log(
+    //     "An error occurred while processing your request. Please verify you have network connectivity and access to Sykes Network"
+    //   );
+    //   console.log("Clicking ~here~ to try again");
+    //   await page.screenshot({
+    //     path: "networkError.png", // Image Dimensions : 800 x 600
+    //   });
+    //   await page.click("a", { delay: 100 }), console.log("here button clicked, going to Login again...");
+      /* ----------------------------------- Filling out username input ----------------------------------- */
+    //   console.log("-- Entering username");
+    //   await page.waitForSelector("#username");
+    //   await page.type("#username", username);
+    //   console.log("\x1b[0m", "Typed username");
+    
+    //   /* ----------------------------------- Filling out Password input ----------------------------------- */
+    //   console.log("-- Entering Password");
+    //   await page.type("#Password", password);
+    //   console.log("\x1b[0m", "Typed Password");
+    
+    //   /* ----------------------------------- Submitting credentials ----------------------------------- */
+    //   console.log("-- Submitting credentials");
+    //   await page.click("#btnLoginButton");
+    //   console.log("Submited credentials!");
+    // }
 
     // If logIn succeds
-    await page.waitForSelector("#divEndSession > a");
+    await page.waitForSelector("#MainTag");
     /* ----------------------------------- Checking if user is logged in ----------------------------------- */
 
     if ((await page.$("#btnLogout")) !== null) {
@@ -75,24 +101,29 @@ const loadTAGWeb = async (req, res) => {
 
     /* ----------------------------------- Logging out  ----------------------------------- */
     isLoggedIn &&
-      (await page.click("#btnLogout", { delay: 1000 }),
+      (await page.click("#btnLogout"),
       console.log("Log Out button clicked"));
 
     /* ----------------------------------- Logging in  ----------------------------------- */
     !isLoggedIn &&
-      (await page.click("#btnLogin", { delay: 1000 }),
+      (await page.click("#btnLogin"),
       console.log("Log In button clicked"));
 
     /* ----------------------------------- Disconnecting session  ----------------------------------- */
+    console.log(Boolean(await page.$(".Processing")) )
+
+    console.log("Waiting for Disconnect button");
     await page.waitForSelector("#divEndSession > a");
-    console.log("Clicking Disconnect button");
-    await page.click("#divEndSession > a", { delay: 200 });
+    let delayInSeconds = 3
+    console.log(`Button found. Clicking Disconnect button in ${delayInSeconds} seconds`);
+    await delay(delayInSeconds)
+    await page.click("#divEndSession > a",);
     console.log("Disconnect button clicked");
 
     await page.screenshot({
       path: "loggedOut.png", // Image Dimensions : 800 x 600
     });
-
+    
     /* ----------------------------------- Closing browser  ----------------------------------- */
     await page.waitForSelector("#btnLoginButton");
     console.log("Logged out. Closing browser");
@@ -101,8 +132,9 @@ const loadTAGWeb = async (req, res) => {
 
     console.log("Browser closed");
   } catch (error) {
-    //   res.json("something bad happened")
-    console.error(error.message);
+      //   res.json("something bad happened")
+      console.error(error);
+      console.error(error.message);
   }
 };
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
-const express = require("express")
-const cors= require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const TAGWebRoute = require("./routes/TAGWebRoute")
+const TAGWebRoute = require("./routes/TAGWebRoute");
 const app = express();
 
-
-const {loadTAGWeb} = require("./controllers/TAGWebController")
-const credentials= require("../credentials.json")
-const {onTime, offTime} = credentials.TAGWeb
+const { loadTAGWeb } = require("./controllers/TAGWebController");
+const credentials = require("../credentials.json");
+const { onTime, offTime } = credentials.TAGWeb;
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -21,26 +20,26 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use(TAGWebRoute)
-const check = () => {
-  console.log("Checking the time...");
-const date = new Date().toTimeString().split(":")
-const time = `${date[0]}:${date[1]} ${date[2].split(" ")[1]}`
-const finalTime = time.split(" ")[0]
-console.log(finalTime);
-if((onTime === finalTime) || (offTime === finalTime) ) {
-  console.log("Bot running...");
-  loadTAGWeb()
-}  
-}
+app.use(TAGWebRoute);
 
-check()
+const checkTime = () => {
+  const date = new Date().toTimeString().split(":");
+  const time = `${date[0]}:${date[1]} ${date[2].split(" ")[1]}`;
+  const finalTime = time.split(" ")[0];
+  console.log("Checking the time every 30 minutes...", finalTime);
+
+  if (onTime === finalTime || offTime === finalTime) {
+    console.log("Bot running...");
+    loadTAGWeb();
+  }
+};
+
+checkTime();
 setInterval(() => {
-  // Check the time every minute
-check()
+  // Check the time every 5 minutes
+  checkTime();
+}, 30*60*1000);
 
-}, 600000);
+app.listen(4200, () => console.log("Server started on port 4200"));
 
-app.listen(4200, () => console.log("Server started on port 4200"))
-
-module.exports = app
+module.exports = app;
